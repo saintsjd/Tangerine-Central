f38ecc3ee833403fe44997d6ba2166e25d8f5584
Jon Saints <saintsjd@gmail.com>
2011-08-28 11:49:22 -0600

    load list of assessments from the DB and store in a Collection

--- _attachments/js/script.js
+++ _attachments/js/script.js
@@ -7,6 +7,24 @@ $(function(){
 
 	//Models
 	window.assessment = new Assessment;
+	var AssessmentList = Backbone.Collection.extend({
+  		model: AssessmentMeta,
+  		
+		comparator: function(meta) {
+		  return meta.get("updated");
+		},
+	});
+	window.assessmentList = new AssessmentList;
+	$.couch.db("tangerine").view("tangerine-cloud" + "/assessment_ids", {
+      success: function(result){
+      	$.each(result.rows, function(key,row){
+      		var a = new AssessmentMeta( {id:row.id, updated:row.key, name: row.value} );
+      		window.assessmentList.add(a);
+      	});
+      }
+    });
+	
+	console.log(window.assessmentList);
 	
 	//Views
 	var metaform = new AssessmentMetaForm({model: window.assessment, el: $('#assessment-meta-form [data-role="content"]') });
