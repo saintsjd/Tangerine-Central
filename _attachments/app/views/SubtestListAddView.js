48fac58e97a768d46d10b1ea22be592dd680d95e
Jon Saints <saintsjd@gmail.com>
2011-09-06 23:30:08 -0600

    save new subtest to database

--- _attachments/app/views/SubtestListAddView.js
+++ _attachments/app/views/SubtestListAddView.js
@@ -7,7 +7,36 @@ var SubtestListAddView = Backbone.View.extend({
 	},
 
 	addNewSubtest: function(e){
-		console.log( $( e.target ).attr("id") );
+		var whichButton = $( e.target ).attr("id");
+		
+		var defaults = {};
+		switch( whichButton ) {
+			case "new-text-page":
+				defaults = {
+					"pageType": "TextPage",
+					"content": "",
+				};
+			break;
+
+			case "new-toggle-grid-with-timer":
+				defaults = {
+					"pageType": "ToggleGridWithTimer",
+					"letters": [],				
+				};			
+			break;
+		}
+		
+		var subtest = new Subtest(defaults);
+		subtest.save({},{
+			success: function(m) {
+				console.log("!!!" + m.get("_id") );
+				//if ( window.assessment.get("urlPathsForPages") == undefined )
+				//	window.assessment.set("urlPathsForPages", [m.get("_id")]);
+				//console.log( window.assessment.get("urlPathsForPages") );
+					
+			},
+		});
+		
 	},
             
 });
