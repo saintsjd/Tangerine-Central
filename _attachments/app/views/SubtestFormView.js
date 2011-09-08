5c180562248a462698f25bcf0324b25394f13a64
Jon Saints <saintsjd@gmail.com>
2011-09-07 22:20:52 -0600

    toggle grid with timer

--- _attachments/app/views/SubtestFormView.js
+++ _attachments/app/views/SubtestFormView.js
@@ -9,8 +9,6 @@ var SubtestFormView = Backbone.View.extend({
   render: function() {
     var type = this.model.get("pageType");
     
-    //this.editView.events = {};
-    
     switch( type ) {
     	case "TextPage":
     		this.template = loadTemplate("TextPageEdit.template.html");
@@ -18,7 +16,12 @@ var SubtestFormView = Backbone.View.extend({
     	break;
     	
     	case "ToggleGridWithTimer":
-    		this.template = loadTemplate("ToggleGridWithTimerEdit.template.html");    	
+    		this.template = loadTemplate("ToggleGridWithTimerEdit.template.html");   
+    		this.events = {
+    			"click button:contains('Sort')" : "sortToggleGridWithTimer",
+    			"click button:contains('Shuffle')" : "shuffleToggleGridWithTimer",
+    			"click button:contains('Save')" : "saveToggleGridWithTimer",
+  			}; 	
     	break;
     
     }
@@ -33,5 +36,37 @@ var SubtestFormView = Backbone.View.extend({
   saveTextPage: function() {
   	this.model.set({"content": this.$("textarea").val()}).save();
   },
+        
+        
+  saveToggleGridWithTimer: function() {
+  	var letters = this.$("textarea").val().split(" ");
+    this.model.set({"letters": letters} ).save();
+  },
+
+  sortToggleGridWithTimer: function() {
+  	var letters = this.$("textarea").val().split(" ");
+    letters.sort();
+    
+    var mush = "";
+    $.each(letters,function(index, value) {
+    	if( value != "")
+    	  mush = mush + value + " ";
+    });
+    this.$("textarea").val(mush);
+  },
+
+  shuffleToggleGridWithTimer: function() {
+  	var letters = this.$("textarea").val().split(" ");
+
+	// quick examples taken from here: http://snippets.dzone.com/posts/show/849
+    for(var j, x, i = letters.length; i; j = parseInt(Math.random() * i), x = letters[--i], letters[i] = letters[j], letters[j] = x);
+
+    var mush = "";
+    $.each(letters,function(index, value) {
+    	if( value != "")
+    	  mush = mush + value + " ";
+    });
+    this.$("textarea").val(mush);
+  },        
            
 });
