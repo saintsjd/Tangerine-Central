d1b3e21174cc1bf98cacb7029861b458b5bd517b
Jon Saints <saintsjd@gmail.com>
2011-09-07 21:04:52 -0600

    theme list dynamically with jquery mobile

--- _attachments/app/views/SubtestListCollectionView.js
+++ _attachments/app/views/SubtestListCollectionView.js
@@ -8,15 +8,26 @@ var SubtestListCollectionView = Backbone.View.extend({
     },
 
 	reset: function() {
-		//this.$(this.el).listview("refresh");
 		$(this.el).empty();
       	return this;
+    	this.$(this.el).listview("refresh");
     },
 
 	addOne: function(subtest) {
 		var view = new SubtestMetaView({model: subtest});
-    	this.$(this.el).prepend(view.render().el);
-    	//this.render();
+    	this.$(this.el).append(view.render().el);
+
+		this.render();		
 	},
+	
+	render: function() {
+    	//not sure why I need to do this. Something conflicts with jquery and backbone... this is a work around
+    	try{ 
+    		this.$(this.el).listview("refresh");
+    	}catch(e){
+    		this.$(this.el).trigger("create");
+    	}
+	
+	}
         
 });
